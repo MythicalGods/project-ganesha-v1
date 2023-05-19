43d42bcb94176df7792e3bdbbd5187567dbc111e
Bruno Gomes <gomes.bruno201@gmail.com>
2023-05-19 17:08:01 -0300

    feat: create index/home template

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from os import getenv
 
@@ -7,10 +7,14 @@ load_dotenv() # Load env variables
 app = Flask(__name__) # __name__ is used to resolve resources
  
 @app.get("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.get("/about")
+def about():
+    return "About me"
 
 if __name__ == "__main__":
     debug = getenv("DEBUG") == "True" # Get debug mode from env variables
 
-    app.run(debug=debug) # Debug mode is enabled
+    app.run(debug=debug) # Debug mode is enabled, rerun the server when you make changes
